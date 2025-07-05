e238de7041d01db5c418f766bf88a5fb2fcbcd39
AngelaEduPe <avergarat@isise.edu.pe>
2025-07-05 01:14:40 -0500

    Creacion de Login

---
+++ ProyectoFinal_MVC/Controllers/LoginController.cs
@@ -0,0 +1,48 @@
+﻿using CapaNegocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoFinal_MVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        private LoginBL loginBL = new LoginBL();
+
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public JsonResult Autenticar(string usuario, string contrasena)
+        {
+            var user = loginBL.VerificarLogin(usuario, contrasena);
+            if (user != null)
+            {
+                Session["Usuario"] = user;
+                return Json(new
+                {
+                    exito = true,
+                    mensaje = "Login correcto",
+                    nombre = user.nombre,
+                    perfil = user.idPerfil
+                });
+            }
+            else
+            {
+                return Json(new
+                {
+                    exito = false,
+                    mensaje = "Usuario o contraseña incorrectos"
+                });
+            }
+        }
+
+
+    }
+}
